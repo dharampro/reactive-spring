5ccac3eba9bbb20bf378796a97b33d3e5ccdbd4f
Dharmendra Kumar Awasthi <dharmendrakumar.awasthi@aristocrat.com>
2021-09-09 19:28:49 +0530

    initialized project

---
+++ src/test/java/com/techendear/api/ReactiveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techendear.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
